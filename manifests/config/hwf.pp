aedb5f382ee8519835308baa2c263711ef18aa30
Luke Harris <luke.harris@katana1.com>
2014-12-16 15:22:50 +1100

    Added Forwarder License

--- manifests/config/hwf.pp
+++ manifests/config/hwf.pp
@@ -15,7 +15,7 @@ class splunk::config::hwf (
     group   => 'splunk',
     mode    => '0644',
     require => Class['splunk::install'],
-  } ->
+  }
   ini_setting { 'Enable Splunk HWF':
     ensure  => present,
     path    => "${SPLUNKHOME}/etc/apps/SplunkForwarder/local/app.conf",
@@ -23,5 +23,26 @@ class splunk::config::hwf (
     setting => 'state',
     value   => $status,
     require => Class['splunk::install'],
+  }
+  exec { "add-forwarder-license":
+    path    => "/sbin:/bin/:/usr/bin:/usr/sbin:/usr/local/bin/",
+    command => "sed -i '$ a\\[license]' /opt/splunk/etc/system/local/server.conf",
+    unless  => "grep '^\[license]' /opt/splunk/etc/system/local/server.conf",
+    require => Class['splunk::install'],
+    notify => Service['splunk'],
+  }
+  exec { "set-forwarder-license":
+    path    => "/sbin:/bin/:/usr/bin:/usr/sbin:/usr/local/bin/",
+    command => "sed -i '/^\[license]/a active_group = Forwarder' /opt/splunk/etc/system/local/server.conf",
+    unless  => "grep '^active_group' /opt/splunk/etc/system/local/server.conf",
+    require => Exec['add-forwarder-license'],
+    notify => Service['splunk'],
+  }
+  exec { "change-forwarder-license":
+    path    => "/sbin:/bin/:/usr/bin:/usr/sbin:/usr/local/bin/",
+    command => "sed -i '/^active_group/s/ = .*/ = Forwarder/' /opt/splunk/etc/system/local/server.conf",
+    unless  => "grep '^active_group = Forwarder' /opt/splunk/etc/system/local/server.conf",
+    require => Exec['set-forwarder-license'],
+    notify => Service['splunk'],
   }
 }
